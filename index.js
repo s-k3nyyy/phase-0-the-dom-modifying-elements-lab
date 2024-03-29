d4503a3d1eead7c5832b29c6cdfcf2a0b7801959
s-k3nyyy <collo.c.heg@gmail.com>
2024-03-29 20:59:31 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+var main = document.getElementById('#main' );
+console.log(main)
+
+const newHeader = document.createElement('h1');
+console.log(newHeader)
+ newHeader.id = 'victory';
+ console.log(newHeader)
+
+newHeader.textContent = 'collins is the champion';
+document.body.appendChild(newHeader);
